b9f641a1de9a2f4eab84e65be51784fd84373dcc
lai2-24 <lai2-24@itismeucci.com>
2023-10-25 09:12:34 +0200

    compito

--- src/main/java/com/itismeucci/stefanelli/AppClient.java
+++ src/main/java/com/itismeucci/stefanelli/AppClient.java
@@ -15,20 +15,37 @@ public class AppClient {
         String ip;
         int port;
 
+        String textInput;
+
         System.out.println("Inserisci l'ip");
         ip = scanner.nextLine();
         System.out.println("Inserisci la porta");
         port = scanner.nextInt();
 
         client.start(ip, port);
+        
 
         System.out.println(client.receive());
         client.send("Connessione riuscita");
 
-        client.send("questo in uppercase");
+        //START OF CODE
+        while(true) {
 
+            textInput = scanner.nextLine();
+
+            if(textInput.toLowerCase().equals("stop")) break;
+            
+            if(textInput.isBlank()) continue;
+
+            client.send(textInput);
+
+            System.out.println(client.receive());
+        }
+
+        client.send("stop");
         System.out.println(client.receive());
-
+        //END OF CODE
+        client.close();
         scanner.close();
     }
 }
--- src/main/java/com/itismeucci/stefanelli/AppServer.java
+++ src/main/java/com/itismeucci/stefanelli/AppServer.java
@@ -2,10 +2,6 @@ package com.itismeucci.stefanelli;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class AppServer 
 {
     public static void main( String[] args )
@@ -14,21 +10,85 @@ public class AppServer
         Server server = new Server();
         int port;
         String inputString;
+        
+        String temp;
+
+        String operator;
+        double operand1;
+        double operand2;
 
         System.out.println("Inserisci la porta su cui aprire, 0 per generarla");
         port = scanner.nextInt();
         server.start(port, System.out);
         
-        server.send("Connessione riuscita");
+        server.send("[SERVER]: Connessione riuscita");
 
         System.out.println(server.receive());
 
-        inputString = server.receive();
+        //START OF CODE
+        while(true) {
 
-        System.out.println("Ricevuto: " + inputString);
+            inputString = server.receive();
 
-        server.send(inputString.toUpperCase());
+            System.out.println("[CLIENT]: " + inputString);
 
+            if (inputString.equals("stop")) {server.send("[SERVER]: Chiusura della Connessione Effettuata"); break;}
+
+            temp = inputString.split("\\+")[0];
+            if(!temp.equals(inputString)) operator = "\\+";
+
+            else {
+
+                temp = inputString.split("-")[0];
+                if(!temp.equals(inputString)) operator = "-";
+
+                else {
+
+                    temp = inputString.split("\\*")[0];
+                    if(!temp.equals(inputString)) operator = "\\*";
+
+                    else {
+
+                        temp = inputString.split("/")[0];
+                        if(!temp.equals(inputString)) operator = "/";
+
+                        else {
+
+                            server.send("[SERVER]: Input non valido - Operatore non valido o mancante");
+                            continue;
+                        }
+                    }
+                }
+            }
+            
+            if(inputString.split(operator).length > 2) {server.send("[SERVER]: Input non valido - Troppi operandi passati"); continue;}
+
+            try {
+                operand1 = Double.parseDouble(inputString.split(operator)[0]);
+                operand2 = Double.parseDouble(inputString.split(operator)[1]);
+            } catch(NumberFormatException e) {server.send("[SERVER]: Input non valido - Uso di caratteri non numerici rilevato"); continue;}
+
+            switch(operator) {
+
+                case "\\+":
+                    server.send("[SERVER]: " + Double.toString(operand1 + operand2));
+                    break;
+                case "-":
+                    server.send("[SERVER]: " + Double.toString(operand1 - operand2));
+                    break;
+                case "\\*":
+                    server.send("[SERVER]: " + Double.toString(operand1 * operand2));
+                    break;
+                case "/":
+                    server.send("[SERVER]: " + Double.toString(operand1 / operand2));
+                    break;
+                default:
+                    server.send("[SERVER]: Input non valido - Questo non dovrebbe mai succedere ma per sicurezza");
+            }
+        }
+        //END OF CODE
+
+        server.close();
         scanner.close();
     }
 }
